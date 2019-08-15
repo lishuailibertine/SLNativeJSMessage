e54b81669d3b8011c226e7634f4dff9744388297
li shuai <lishuai19@yeah.net>
2019-08-15 19:05:11 +0800

    Remove

--- Example/SLNativeJSMessage/SLAppDelegate.m
+++ Example/SLNativeJSMessage/SLAppDelegate.m
@@ -13,9 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    //ceshi1
-    //ceshi2
-    //ceshi3
     return YES;
 }
 
